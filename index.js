8d16dd146873a57854ed302d2748f7f6244c0322
Ian Shoemaker <ishoemaker@candoris.com>
2016-07-11 16:29:24 -0400

    1.0.0

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const transform = require('stream-transform');
 const mware = require('mware')();
 
 module.exports = function (config) {
+  if (!config.filePath) {
+    throw new Error('config.filePath Required for innie');
+  }
+  if (!config.end) {
+    throw new Error('config.end Required for innie');
+  }
+  if (!config.writer) {
+    throw new Error('config.writer Required for innie');
+  }
   const input = fs.createReadStream(config.filePath);
   return {
     registerMiddleware: mware,
